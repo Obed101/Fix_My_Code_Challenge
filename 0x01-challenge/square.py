36d7a81a5ae5feedb71c4552774958f676681b1d
Obed101 <obedamoako92@gmail.com>
2022-02-14 17:56:18 +0000

    documentations

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-""" This is a square class I'm debugging """
+"""
+This is a square class I'm debugging
+"""
 
 
 class square():
-    """Printing a square"""
+    """ Printing a square """
 
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """instance initializer"""
+        """ instance initializer """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -18,16 +20,16 @@ class square():
         return self.height * self.width
 
     def PermiterOfMySquare(self):
-        """perimeter printer"""
+        """ perimeter printer """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """Returning the str format"""
+        """ Returning the str format """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """driver code"""
+    """ driver code """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
